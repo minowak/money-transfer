867c4d9e9bf8af0e05d9db6af6d6059a25c64602
Michał Nowak <alejajestemmondry@gmail.com>
2018-09-20 11:31:34 +0200

    Implemented AccountController

--- src/main/java/com/minowak/api/AccountsController.java
+++ src/main/java/com/minowak/api/AccountsController.java
@@ -1,6 +1,7 @@
 package com.minowak.api;
 
 import com.minowak.model.Account;
+import com.minowak.service.AccountsService;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -8,41 +9,47 @@ import javax.ws.rs.core.Response;
 
 @Path("/user/account")
 public class AccountsController {
+    private final AccountsService accountsService = AccountsService.getInstance();
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Iterable<Account> getAccounts() {
-        return null;
+        return accountsService.get();
     }
 
     @GET
     @Path("{number}")
     @Produces(MediaType.APPLICATION_JSON)
     public Account getAccount(@PathParam("number") String number) {
-        return null;
+        return accountsService.get(number);
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createAccount(Account account) {
-        return null;
+        boolean created = accountsService.add(account);
+        return created ? Response.status(Response.Status.CREATED).build()
+                : Response.status(Response.Status.CONFLICT).build();
     }
 
     @PUT
     @Path("{number}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response updateAccount(Account account) {
-        return null;
+    public Response updateAccount(Account account, @PathParam("number") String number) {
+        accountsService.update(number, account);
+        return Response.status(Response.Status.CREATED).build();
     }
 
     @DELETE
     public Response deleteAllAccounts() {
-        return null;
+        accountsService.delete();
+        return Response.status(Response.Status.GONE).build();
     }
 
     @DELETE
     @Path("{number}")
     public Response deleteAccount(@PathParam("number") String number) {
-        return null;
+        accountsService.delete(number);
+        return Response.status(Response.Status.GONE).build();
     }
 
 }
